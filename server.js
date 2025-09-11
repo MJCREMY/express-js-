99400bef16d430b22e0d7b8b688969f4748d9bb8
Jeyep <mjfbaltazar1@tip.edu.ph>
2025-09-11 15:34:36 +0800

    maangas na front end

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
 
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+const postsFilePath = path.join(__dirname, 'data', 'posts.json');
+
+app.get('/blog-data', (req, res) => {
+  fs.readFile(postsFilePath, 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: 'Failed to load posts' });
+    }
+    res.json(JSON.parse(data));
+  });
 });
 
-app.get('/about', (req, res) => { 
-    res.sendFile(path.join(__dirname, 'views', 'about.html'));
+
+app.get('/blog', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'blog.html'));
 });
 
-app.get('/contact', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'contact.html'));
-});
-
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
